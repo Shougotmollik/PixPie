58b4a6db5dc70da21761b7eb8c91faf0cce090bb
ShougotMollik <shougotmollik@gmail.com>
2024-12-22 22:13:03 +0600

    - design Home screen - fetch image

--- lib/services/app_url.dart
+++ lib/services/app_url.dart
@@ -1,5 +1,9 @@
 class AppUrl {
   static String getImageUrl(int index) {
-    return 'https://picsum.photos/500/800?random=$index';
+    return 'https://picsum.photos/500/800?random=img_$index';
+  }
+
+  static String getSliderImageUrl(int index) {
+    return 'https://picsum.photos/500/800?random=slider_$index';
   }
 }
