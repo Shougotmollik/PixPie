4894962c3b061254e7b48c1759504e374cade6d8
ShougotMollik <shougotmollik@gmail.com>
2024-12-27 21:50:45 +0600

    - design explorer screen - fetch image

--- lib/widgets/image_tile.dart
+++ lib/widgets/image_tile.dart
@@ -13,45 +13,95 @@ class ImageTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(8))),
-      clipBehavior: Clip.antiAlias,
-      height: extent,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          CachedNetworkImage(
-            imageUrl: imageSource,
-            fit: BoxFit.cover,
-          ),
-          Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
-                color: const Color.fromARGB(179, 0, 0, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Art $index",
-                      style: const TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.w600),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.favorite_outline,
-                        color: Colors.white,
+    return GestureDetector(
+      onTap: () {
+        _showImageDetails(context, imageSource, index);
+      },
+      child: Container(
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(8))),
+        clipBehavior: Clip.antiAlias,
+        height: extent,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            CachedNetworkImage(
+              imageUrl: imageSource,
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+                  color: const Color.fromARGB(179, 0, 0, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Art $index",
+                        style: const TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w600),
                       ),
-                    )
-                  ],
-                ),
-              ))
-        ],
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.favorite_outline,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ))
+          ],
+        ),
       ),
+    );
+  }
+
+  void _showImageDetails(BuildContext context, String imageSource, int index) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(14))),
+      clipBehavior: Clip.antiAlias,
+      builder: (context) {
+        return FractionallySizedBox(
+          heightFactor: 0.90,
+          child: Column(
+            children: [
+              CachedNetworkImage(imageUrl: imageSource),
+              Text(
+                'Art $index',
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black54,
+                ),
+              ),
+              MaterialButton(
+                onPressed: () {},
+                color: Colors.black,
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(8),
+                minWidth: MediaQuery.of(context).size.width * 0.5,
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(08))),
+                child: const Text(
+                  'Get Wallpaper',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
